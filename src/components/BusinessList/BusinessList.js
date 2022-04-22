dfe2bb8b513d3fe964259d4d1a04c5675a3636a1
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-04-22 08:36:18 -0500

    passing information across components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,16 +2,33 @@ import React from "react";
 import './BusinessList.css';
 import Business from '../Business/Business'
 
+// class BusinessList extends React.Component {
+//     render() {
+//         return (<div className="BusinessList">
+//             {
+//                 this.props.business.map((business) => {
+//                     return <Business business={business} />
+//                 )}
+//                 }
+//                 }
+//             }
+
+//         </div>)
+//     }
+
 class BusinessList extends React.Component {
     render() {
-        return (<div className="BusinessList">
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-        </div>)
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.business.map(
+                        (business) => {
+                            return <Business business={business} />
+                        }
+                    )
+                }
+            </div>
+        )
     }
 }
 
