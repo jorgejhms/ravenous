4f9b08eebb3324d8696d72848717014842670880
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-04-24 17:29:07 -0500

    Incorporando modulo Yelp

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,30 @@
 const apiKey = 'nxmvBqjWbD5rA-usQ_NvcwqmwY8uG4JVCJbplu4VF4txVMWGSFXGVgMWjRUJSeJAFgXwV3xnSJ2MpWxygKHzAfHgaxsM-HbFi5MAO8w4XQjP0MEBrbFoS3n8PMNlYnYx'
+
+export const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+        return response.json();
+      }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.address,
+              city: business.city,
+              state: business.state,
+              zipCode: business.zipCode,
+              category: business.category,
+              rating: business.rating,
+              reviewCount: business.reviewCount
+            }
+          });
+        }
+      });
+  }
+}
