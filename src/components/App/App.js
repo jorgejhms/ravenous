aa63783b751d639062950ab737d708e3ce12a0b0
Jorge Meneses Saco <jorgejhms@gmail.com>
2022-04-24 16:41:12 -0500

    Añadiendo whatwg

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
+import { render } from '@testing-library/react';
 
 const business = {
   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -25,14 +26,21 @@ let businessArr = [
   business
 ]
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList business={businessArr}/>
-    </div>
-  );
+class App extends React.Component {
+  
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList business={businessArr} />
+      </div>
+    );
+  }
 }
 
 export default App;
